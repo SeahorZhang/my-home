bd822ec9e5cdb2a774e8a32a10a10f6f914eef42
xihe <im0k@outlook.com>
2024-02-12 12:43:54 +0800

    更改样式

--- toolSoftware/overview.vue
+++ toolSoftware/overview.vue
@@ -6,17 +6,18 @@ import { withBase } from 'vitepress'
 
 <template>
   <template v-for="(list, i) in data" :key="i">
-    <h3 style="margin-bottom: 12px">{{ list.text }}</h3>
+    <h3 style="margin-bottom: 12px"> {{ list.text }} </h3>
     <div class="grid">
-      <a v-for="(item, j) in list.items" :key="j" :href="withBase(list.base + item.link)">
+      <a v-for="(item, j) in list.items" :key="j" class="item" :href="withBase(list.base + item.link)">
+        <div class="tip">
+          <span>{{ item.type }}</span>
+        </div>
         <el-card class="card" shadow="hover">
           <h3>{{ item.text }}</h3>
           <el-row justify="center">
-    <!-- <Badge v-for="(tag,i) in item.tag" :key="i" :type="tag.type" :text="tag.text" /> -->
-            <el-tag v-for="tag in item.tag" class="tag" :type="tag.type">
+            <el-tag v-for="tag in item.tag" class="tag" :type="tag.type==='tip'?'success':tag.type">
               {{ tag.text}}
             </el-tag>
-            <el-tag class="tag">{{ item.type }}</el-tag>
           </el-row>
         </el-card>
       </a>
@@ -29,6 +30,25 @@ import { withBase } from 'vitepress'
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 8px;
+}
+
+.grid .item{
+  position: relative;
+}
+
+.grid .item .tip{
+  position: absolute;
+  top:0;
+  right:0;
+  font-size: 12px;
+  background-color: #409EFF;
+  border-radius: 0 4px 0 4px;
+  padding:4px 6px;
+  color:#fff;
+  line-height: 1;
+}
+.grid .item .tip >span:not(:last-child){
+  margin-right: 4px;
 }
 .grid a {
   cursor: pointer;
